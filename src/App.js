a5197db32b471558feac997529b138dd0500d780
VRAJ9R5 <rahul.raj2@pearson.com>
2024-01-25 20:06:18 +0530

    ROuting way 2

--- src/App.js
+++ src/App.js
@@ -3,25 +3,25 @@ import NavBar from "./components/NavBar";
 import Home from "./components/Home";
 import About from "./components/About";
 import Item from "./components/Item";
-import { createBrowserRouter,RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
 export default function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Home />,
-    },
-    {
-      path: "/about",
-      element: <About />,
-    },
-    {
-      path: "/items",
-      element: <Item />,
-    },
-  ]);
+  const routes = createRoutesFromElements(
+    <>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/items" element={<Item />} />
+    </>
+  );
+  const router = createBrowserRouter(routes);
   return (
     <>
-      <RouterProvider router={router}/>
+    <NavBar/>
+      <RouterProvider router={router} />
     </>
   );
 }
